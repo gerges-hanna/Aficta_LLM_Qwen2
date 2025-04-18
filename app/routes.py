d62da6b9f6f9c75423b5583559e608ce81ca866e
Gerges Hanna <gerges.hanna@initiumsolutions.me>
2025-04-18 03:26:09 +0200

    add airline code prediction 3

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,7 @@ def predict():
         return jsonify({"error": "Missing 'query' in request"}), 400
 
     model, tokenizer = get_model(lora_name)
-    if(lora_name=="airline_code")
+    if(lora_name=="airline_code"):
         result=predict_airline_code(model,tokenizer,user_input)
     else:
         result = generate_output(model, tokenizer, query)
